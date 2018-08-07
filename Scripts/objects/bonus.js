de30e15b109a3f2340139f2182ba9cb9760cc1d9
piyushsehli <piyushsehli@gmail.com>
2018-08-06 23:38:22 -0400

    Final Release: Adding comments

--- Scripts/objects/bonus.js
+++ Scripts/objects/bonus.js
@@ -8,6 +8,43 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+/*
+
+     
+  Author’s name: Piyush Sehli, Chandni Patel, Khusboo Sakervala,
+  Student Number: 300984528, 300990555, 300984318
+  Last Modified by: Piyush, Khusboo and Chandni
+  Date last Modified: 6 August, 2018,
+  Program description: -	Marino is Bullet Hell Shooter, where tin-tin (sea-horse) want to live long by eating fish, keep it self safe from shark, water dragons and killing small fishes.
+-	The Goal of this game is to pass all the levels. ,
+  Revision History:
+  Final Release: Last changes
+
+@piyushsehli
+piyushsehli committed 22 minutes ago
+ 
+Final Release: Updating score system
+
+@piyushsehli
+piyushsehli committed an hour ago
+ 
+Final Release: adding rules screen
+
+@piyushsehli
+piyushsehli committed 2 hours ago
+ 
+Final Release: Adding level screens and actions
+
+@piyushsehli
+piyushsehli committed 2 hours ago
+ 
+Final Release: changing characters
+
+@piyushsehli
+piyushsehli committed 3 hours ago
+ 
+
+*/
 var objects;
 (function (objects) {
     var Bonus = /** @class */ (function (_super) {
--- Scripts/objects/scene.ts
+++ Scripts/objects/scene.ts
@@ -1,3 +1,41 @@
+/*
+
+     
+  Author’s name: Piyush Sehli, Chandni Patel, Khusboo Sakervala, 
+  Student Number: 300984528, 300990555, 300984318 
+  Last Modified by: Piyush, Khusboo and Chandni 
+  Date last Modified: 6 August, 2018,
+  Program description: -	Marino is Bullet Hell Shooter, where tin-tin (sea-horse) want to live long by eating fish, keep it self safe from shark, water dragons and killing small fishes.
+-	The Goal of this game is to pass all the levels. , 
+  Revision History:
+  Final Release: Last changes
+
+@piyushsehli
+piyushsehli committed 22 minutes ago
+ 
+Final Release: Updating score system
+
+@piyushsehli
+piyushsehli committed an hour ago
+ 
+Final Release: adding rules screen
+
+@piyushsehli
+piyushsehli committed 2 hours ago
+ 
+Final Release: Adding level screens and actions
+
+@piyushsehli
+piyushsehli committed 2 hours ago
+ 
+Final Release: changing characters
+
+@piyushsehli
+piyushsehli committed 3 hours ago
+ 
+
+*/
+
 module objects {
     export abstract class Scene extends createjs.Container {
         // member variables
